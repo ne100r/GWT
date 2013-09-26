0fbf9c47fdc8f72c7f0c97349b7cf67f7ae31a52
ne100r <alexey.ovsienko@gmail.com>
2013-09-26 23:50:55 +0400

    Hello World App

---
+++ gwtia-ch02-helloworld/src/com/manning/gwtia/ch02/client/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.manning.gwtia.ch02.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class HelloWorld implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Label theGreeting = new Label("Hello World!");
+		RootPanel.get().add(theGreeting);
+	}
+
+}
