60151fc93811eec189ccb68f1f69fd9c0d281f77
ne100r <alexey.ovsienko@gmail.com>
2013-09-29 00:55:11 +0400

    deploy hello world app

--- gwtia-ch02-helloworld/src/com/manning/gwtia/ch02/client/HelloWorld.java
+++ gwtia-ch02-helloworld/src/com/manning/gwtia/ch02/client/HelloWorld.java
@@ -1,8 +1,10 @@
 package com.manning.gwtia.ch02.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 public class HelloWorld implements EntryPoint {
 
@@ -10,6 +12,10 @@ public class HelloWorld implements EntryPoint {
 	public void onModuleLoad() {
 		Label theGreeting = new Label("Hello World!");
 		RootPanel.get().add(theGreeting);
+		RootPanel.get().add(new Button("Click Me!"));
+		SimplePanel testPanel = new SimplePanel();
+		testPanel.add(new Label("A label in a panel"));
+		RootPanel.get().add(testPanel);
 	}
 
 }
